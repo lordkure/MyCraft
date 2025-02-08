f83fb1c65f0dbf4a22abf1ac43729ce8cbb911e0
Cleber Luiz da Silva dos Santos <cleberluizdasilvadossantos@gmail.com>
2025-02-08 12:26:39 -0300

    feat: remake ChunkRenderer

--- src/main/java/com/mycraft/render/ChunkRenderer.java
+++ src/main/java/com/mycraft/render/ChunkRenderer.java
@@ -4,7 +4,7 @@ import static org.lwjgl.opengl.GL30.*;
 
 public class ChunkRenderer {
 
-    private int vao, vbo;
+    private int vao, vbo, ebo;
 
     private static final float[] VERTICES = {
             // Posição          // Coordenadas de Textura
@@ -40,12 +40,27 @@ public class ChunkRenderer {
             -0.5f, -0.5f,  0.5f,  0.0f, 1.0f,
     };
 
+    private static final int[] INDICES = {
+            0, 1, 2, 2, 3, 0, // Frente
+            4, 5, 6, 6, 7, 4, // Trás
+            8, 9, 10, 10, 11, 8, // Superior
+            12, 13, 14, 14, 15, 12, // Inferior
+            16, 17, 18, 18, 19, 16, // Direita
+            20, 21, 22, 22, 23, 20  // Esquerda
+    };
+
+
     public void create() {
         vao = glGenVertexArrays();
         vbo = glGenBuffers();
+        ebo = glGenBuffers();
 
         glBindVertexArray(vao);
+
         glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, VERTICES, GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ARRAY_BUFFER, ebo);
         glBufferData(GL_ARRAY_BUFFER, VERTICES, GL_STATIC_DRAW);
 
         // Configura a posição dos vértices (location = 0)
@@ -62,18 +77,13 @@ public class ChunkRenderer {
 
     public void render() {
         glBindVertexArray(vao);
-        glEnableVertexAttribArray(0); // Ativa o atributo de posição
-        glEnableVertexAttribArray(1); // Ativa o atributo UV
-
-        glDrawArrays(GL_QUADS, 0, 24); // Desenha as 6 faces do cubo
-
-        glDisableVertexAttribArray(0);
-        glDisableVertexAttribArray(1);
+        glDrawElements(GL_TRIANGLES, INDICES.length, GL_UNSIGNED_INT, 0); // Desenha as 6 faces do cubo
         glBindVertexArray(0);
     }
 
     public void cleanup() {
         glDeleteBuffers(vbo);
+        glDeleteBuffers(ebo);
         glDeleteVertexArrays(vao);
     }
 }
